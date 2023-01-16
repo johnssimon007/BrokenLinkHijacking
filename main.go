cdb66c6322d90e89857ac33f661cf2ff2802f53d
unknown <johnssimon007@gmail.com>
2023-01-16 12:15:29 +0530

    major fixes

--- main.go
+++ main.go
@@ -1,92 +1,96 @@
 package main
 
 import (
-	"crypto/tls"
-	"flag"
-	"fmt"
-	"github.com/jackdanger/collectlinks"
-	"net/http"
-	"github.com/gookit/color"
-  "strings"
-	"net/url"
-	"os"
+    "crypto/tls"
+    "flag"
+    "fmt"
+    "github.com/jackdanger/collectlinks"
+    "net/http"
+    "github.com/gookit/color"
+    "strings"
+    "net/url"
+    "os"
 )
 
+var sem = make(chan struct{}, 20)
+
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: BrokenLinkHijacking https://example.com\n")
-	flag.PrintDefaults()
-	os.Exit(2)
+    fmt.Fprintf(os.Stderr, "usage: BrokenLinkHijacking https://example.com\n")
+    flag.PrintDefaults()
+    os.Exit(2)
 }
 
 func main() {
-	flag.Usage = usage
-	flag.Parse()
+    flag.Usage = usage
+    flag.Parse()
 
-	args := flag.Args()
-	fmt.Println(args)
-	if len(args) < 1 {
-		usage()
-		fmt.Println("Please specify start page")
-		os.Exit(1)
-	}
+    args := flag.Args()
+    fmt.Println(args)
+    if len(args) < 1 {
+        usage()
+        fmt.Println("Please specify start page")
+        os.Exit(1)
+    }
 
-	queue := make(chan string)
-	filteredQueue := make(chan string)
+    queue := make(chan string)
+    filteredQueue := make(chan string)
 
-	go func() { queue <- args[0] }()
-	go filterQueue(queue, filteredQueue)
+    go func() { queue <- args[0] }()
+    go filterQueue(queue, filteredQueue)
 
+    done := make(chan bool)
 
-	done := make(chan bool)
-
-	for i := 0; i < 100; i++ {
-		go func() {
-			for uri := range filteredQueue {
-				enqueue(uri, queue)
-			}
-			done <- true
-		}()
-	}
-	<-done
+    for i := 0; i < 150; i++ {
+        go func() {
+            sem <- struct{}{}
+            for uri := range filteredQueue {
+                enqueue(uri, queue)
+            }
+            <-sem
+            done <- true
+        }()
+    }
+    <-done
 }
 
 func filterQueue(in chan string, out chan string) {
-	var seen = make(map[string]bool)
-	for val := range in {
-		if !seen[val] {
-			seen[val] = true
-			out <- val
-		}
-	}
+    var seen = make(map[string]bool)
+    for val := range in {
+        if !seen[val] {
+            seen[val] = true
+            out <- val
+        }
+    }
 }
 
 func enqueue(uri string, queue chan string) {
-	fmt.Println("fetching", uri)
-  transport := &http.Transport{
-    TLSClientConfig: &tls.Config{
-      InsecureSkipVerify: true,
-    },
-  }
-  client := http.Client{Transport: transport}
-	resp, err := client.Get(uri)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
+    fmt.Println("fetching", uri)
+    transport := &http.Transport{
+        TLSClientConfig: &tls.Config{
+            InsecureSkipVerify: true,
+        },
+    }
+    client := http.Client{Transport: transport}
+    resp, err := client.Get(uri)
+    if err != nil {
+        return
+    }
+    defer resp.Body.Close()
 
-	links := collectlinks.All(resp.Body)
-  status_codes := map[int]string{
-     404: "Resource Not FOUND",
-  }
-  domain_list := map[string]string{
-     "linkedin.com":  "Linkedin",
-     "facebook.com":  "Facebook",
-     "twittter.com":  "twitter",
-     "youtube.com":   "youtube",
-     "twitch.com":    "twitch",
-     "discord.com":   "discord",
-     "instagram.com": "instagram",
-  }
+    links := collectlinks.All(resp.Body)
+    status_codes := map[int]string{
+        404: "Resource Not FOUND",
+    }
+    domain_list := map[string]string{
+        "linkedin.com":  "Linkedin",
+        "facebook.com":  "Facebook",
+        "twittter.com":  "twitter",
+        "youtube.com":   "youtube",
+        "twitch.com":    "twitch",
+        "discord.com":   "discord",
+        "instagram.com": "instagram",
+    }
+
 
 	for _, link := range links {
 		absolute := fixUrl(link, uri)
